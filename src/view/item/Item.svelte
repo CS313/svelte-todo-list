58b517d5f7cb4659e5d728b0e932b42ef463055a
katsiaryna-savchuk-itechart <katsiaryna.savchuk@itechart-group.com>
2021-03-29 20:32:06 +0300

    add animation

--- src/view/item/Item.svelte
+++ src/view/item/Item.svelte
@@ -1,12 +1,13 @@
 <script>
   import moment from 'moment';
+  import { fly } from 'svelte/transition';
   import './style.css';
 
   export let item;
   export let deleteNote;
 </script>
 
-<div class="list-item">
+<div class="list-item" transition:fly={{ y: 200, duration: 2000 }}>
   <div class="list-item-content">
     <p>{item.note}</p>
     <div><em><small>{moment(new Date(item.time)).fromNow()}</small></em></div>
